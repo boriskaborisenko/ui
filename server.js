afc0edc1ecc05fba2de8aea2f08afefe322c2868
Unknown <boriskaborisenko@gmail.com>
2018-08-03 12:58:53 +0300

    ffff

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.get('/',function(req,res){
-    res.sendFile(__dirname+'/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.use('/css',express.static(__dirname + '/css'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/images', express.static(__dirname + '/images'));
 
 app.listen(3232, () => console.log('Example app listening on port 3232!'))
